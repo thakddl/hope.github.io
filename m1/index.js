b9bde5c701b789e847ae98e7e9a509517fee2dd5
thakddl <55184246+thakddl@users.noreply.github.com>
2019-11-29 15:01:20 +0900

    to rename

---
+++ m1/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    function ani(){
+        var a = $("p:first").outerWidth(true);
+        $("#res1").text("마진을 포함한 너비 : "+a);
+        $("#res2").text("마진을 포함한 높이 : "+b);
+        );
+    }
+});
