ef12812d4774db2b65d8841cec3579fde9c68102
thakddl <55184246+thakddl@users.noreply.github.com>
2019-11-30 19:08:09 +0900

    index slide jQuery

--- m1/index.js
+++ m1/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     function ani(){
-        var a = $("p:first").outerWidth(true);
-        $("#res1").text("마진을 포함한 너비 : "+a);
-        $("#res2").text("마진을 포함한 높이 : "+b);
-        );
-    }
+        var a = parseInt($(".vs_fr li").width());
+        $(".vs_fr").not(":animated").animate({"margin-left": -a+"px"}, 1000, function(){
+            $(".vs_fr li").eq(0).appendTo($(".vs_fr"));
+            $(".vs_fr").css("margin-left","0");
+        });
+    } setInterval(function(){ ani(); }, 3000);
 });
