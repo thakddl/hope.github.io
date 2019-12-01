e29c8d069c7e19dbbb2b6622c86883f471d74f80
thakddl <55184246+thakddl@users.noreply.github.com>
2019-12-01 20:50:04 +0900

    Add files via upload

---
+++ m1/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#t_btn2").click(function(){
+        $("#ck1").prop("checked",false);
+    });
+    $("#t_btn1").click(function(){
+        $("#ck2").prop("checked",false);
+    });
+});
