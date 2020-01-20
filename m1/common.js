ad5c979eeb9d8696d98cd5e75ee1fc5272025bfd
thakddl <55184246+thakddl@users.noreply.github.com>
2020-01-20 11:18:08 +0900

    Add files via upload

--- m1/common.js
+++ m1/common.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
+    sw=1
     $("#t_btn2").click(function(){
         $("#ck1").prop("checked",false);
     });
     $("#t_btn1").click(function(){
+        sw=1
         $("#ck2").prop("checked",false);
+        if(sw==1){$("#sign").removeClass("fa-sign-out-alt").addClass("fa-sign-in-alt");}
     });
+    $("#t_btn2").click(function(){
+        if(sw==1){$("#sign").removeClass("fa-sign-in-alt").addClass("fa-sign-out-alt");}
+        else {$("#sign").removeClass("fa-sign-out-alt").addClass("fa-sign-in-alt");}
+        sw*=-1
+    })
 });
