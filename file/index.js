9c58f5a36850c4ecae644c1143c83fe520eac379
thakddl <55184246+thakddl@users.noreply.github.com>
2020-01-21 18:12:55 +0900

    Add files via upload

--- file/index.js
+++ file/index.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
   //menu call
   var sw=true;
-  $("#moon").click(function(){
-    $("#info").fadeOut(500);
+  var ht = $(window).height(); 
+  function menu()
+  {
     if(sw==true){
-      $("#moon").css("animation", "circle 6s infinite linear");
+      $("#moon").css("animation", "circle 4s 1 linear");
       for(var a=0; a<5; a++){
         $(".gnb li:eq("+a+")").css({"transform":"translateX(0px)", "opacity":"1", "transition-delay":a*0.4+"s"});
       }
     } else{
-      $("#moon").css("animation", "scale 4s infinite linear");
+      $("#moon").css("animation", "scale 5s infinite linear");
       var d = 0;
       for(var a=4; a>-1; a--){
         d++
@@ -17,19 +18,65 @@ $(document).ready(function(){
         );
       }
     } sw=!sw;
+  }
+  function fishing()
+  {
+    var st = $("body").scrollTop(); //html은 overflow hidden상태이므로 스크롤 불가능 
+    var idx = st/ht;
+    tar = $(".gnb li:eq("+idx+")");
+    tar_idx = tar.index();
+    var lf=tar.offset().left;
+    var wd=tar.width();
+      $("#fishing").stop().fadeIn(500).css( {"left": lf+wd-10+"px"} );
+      $("#float").stop().fadeOut(1000);
+  }
+  //fishing first position
+  var st = $("body").scrollTop(); //html은 overflow hidden상태이므로 스크롤 불가능 
+  var idx = st/ht;
+  tar = $(".gnb li:eq("+idx+")");
+  tar_idx = tar.index();
+  var lf=tar.offset().left;
+  var wd=tar.width();
+  $("#fishing").stop().css( {"left": lf+wd-10+"px"} );
+  //moon cilck effect
+  $("#moon").click(function(){
+    $("#info").fadeOut(500);
+    if ( sw ){
+      $("#hide").css("display","block");
+      setTimeout(menu,10);
+      setTimeout(fishing,3000);
+    } 
+    else { menu();
+      setTimeout(function(){$("#fishing").fadeOut();},1500);
+      setTimeout(function(){$("#hide").css("display","none");}, 2400); 
+    }
   });
-  // menu btn
+  //scroll effect
+  $("body, html").scroll(function() { 
+    if(sw==false){fishing();}
+  });
+
+  // gnb li cilck & hover effect
   $(".gnb li").click(function(){
     var idx = $(this).index();
     $("body, html").not(":animated").animate({"scrollTop":idx*ht+"px"}, 900, 'easeOutQuad');
   });
-
-  $("#ability svg").css({"position":"absolute", "cursor":" pointer"});
-  $("#ability svg").hover(function(){
-    $(".planet_tit")
+  $(".gnb li").mouseover(function(){
+    var lf=$(this).offset().left;
+    var wd=$(this).width();
+    var idx = $(this).index();
+    var st = $("body").scrollTop();
+    var sc_idx = st/ht;
+    if( idx==sc_idx ) { $("#float").stop().fadeIn(200).css({"animation":"rotate 2s infinite linear", "left": lf-50+"px"}) }
+    else {
+      $("#float").stop().fadeIn(200).css({"animation":"float 2s infinite linear", "left": lf+wd+10+"px"});
+    }
   });
+  $(".gnb li").mouseout(function() {
+    $("#float").stop().fadeOut(200);
+  });
+
   // mousewheel
-  var ht = parseInt($(window).height());
   $(window).on("mousewheel", function(e){
     if(e.originalEvent.wheelDelta < 0){
       $("#info").fadeOut(500);
@@ -38,24 +85,25 @@ $(document).ready(function(){
       $("html, body").not(":animated").animate({"scrollTop":"-="+ht+"px"}, 900, 'easeOutQuad')
     }
   });
+
 //planets control
-  sw=false;
+  sw2=false;
   $("#ring").click(function(){
-    sw=true;
-    $("#ring_wrap").css({"width":"25vh", "heigth":"25vh", "top":"20%", "left":"15%" });
+    sw2=true;
+    $("#ring_wrap").css({"width":"25vh", "heigth":"25vh", "top":"22%", "left":"15%" });
     $("#ring_tit").text("Cilck planets.");
     $("#planet_box").css({"width":"87%", "transform":"translate(0%)"});
     $("#ability_box li").stop().hide(1000); 
   });
   $(".planet_wrap").click(function(){
-    sw=false;
+    sw2=false;
     $("#ring_wrap").css({"width":"21vh", "heigth":"21vh", "top":"17%", "left":"9%"});
     var name = document.getElementById("ring_tit");
     var idx = $(this).index();
     var arr = [90,85,73,85,65,72];
-    var score = arr[idx]; var x = 55;
+    var score = arr[idx]; var x = 40;
     function counter(){
-    if(sw==true){ return false; }//or x=score;
+    if(sw2==true){ return false; }//or x=score;
     else if ( x++<score ) {
       name.innerText = x+"%";
       setTimeout(counter,1);
