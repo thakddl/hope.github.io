508e034709a4938cdaa6dd7152d5974d84877b27
HopeLee <coidss22@gmail.com>
2020-02-26 14:19:59 +0900

    add meta tag and organize index.js file

--- file/index.js
+++ file/index.js
@@ -128,6 +128,85 @@ $(document).ready(function(){
   $(".gnb li").mouseout(function() {
     $("#float").stop().fadeOut(200);
   });
+  
+  //scroll effect 
+  $("body").scroll(function() { 
+    if(sw==true){ setTimeout(function() { fishing(); }, 920); }
+    $("#info").fadeOut(500);
+
+    abOfs = $("#about").offset().top;
+    if( 0<=abOfs){
+      function pl(){
+        $("#ab_box").fadeTo(300,1);
+        for (id=-1; id<=4; id++ ){
+          $("#card li.card_lst:eq("+id+")").css("transition-delay",id*0.2+"s");
+          $("#card li.card_lst:eq("+id+")").addClass("on");
+        } 
+      }setTimeout(function () { pl(); },600);
+    } 
+    
+    alOfs = $("#ability").offset().top;
+    var scr = parseInt($("body").width());
+    if(scr>=1200 && 0!=alOfs){
+      ring_reset();
+    }
+
+    conOfs = $("#contact").offset().top;
+      if(0==conOfs){
+      $("#rocket").css("animation","rocket 8s ease-out infinite");
+    } 
+  });
+  
+  //planets control
+  sw2=true;//점수 멈춤 기능
+  function ring_reset()//
+  { 
+    sw2=false;
+    var scr = parseInt($("body").width());
+    if (scr>=1200){
+      $("#ring_tit").text("Cilck planets.");
+      $("#ability_box li").stop().css("display","none"); 
+      $("#ring_wrap").css({"width":"25vh", "heigth":"25vh", "top":"22%", "left":"15%" });
+      $("#planet_box").css({"width":"87%", "transform":"translate(0%)"});
+    }
+    else {
+      $("#ring_tit").text("Cilck planets.");
+      $("#ability_box li").stop().css("display","none"); 
+    }
+  }
+
+  // 링클릭시 ability 화면 리셋
+  $("#ring").click(function(){
+    ring_reset();
+  });
+
+  $(".planet_wrap").click(function(){
+    sw2=true;
+    var scr = parseInt($("body").width());
+    if (scr>=1200){
+      $("#ring_wrap").css({"width":"21vh", "heigth":"21vh", "top":"17%", "left":"9%"});
+      $("#planet_box").css({"width":"60%", "transform":"translate(25%)"});
+    } 
+    
+    var name = document.getElementById("ring_tit");
+    var idx = $(this).index();
+    var arr = [95,92,"developing..",88,65,72];
+    var score = arr[idx]; var x = 30;
+    function counter(){
+      if(sw2==false){ return false; }//or x=score;
+      else {
+        if ( typeof score != typeof x ) { //문자일 경우(숫자가 아닐경우)
+          name.innerText = score;
+        } else if( x++<score ){
+          name.innerText = x+"%";
+          setTimeout(counter, 1);
+        }
+      }  
+    } counter();
+    
+    $("#ability_box li.ability_con:eq("+idx+")").stop().slideDown(1000);
+    $("#ability_box li.ability_con:eq("+idx+")").siblings().stop().css("display","none");
+  });
 
   //mobile ability click effect
   $(".ability_works dt").click(function(){
@@ -161,84 +240,6 @@ $(document).ready(function(){
     $("#real").css({"display":"none"});
     $("#charlee").css({"display":"block"});
     $("#hover").text("Mouseover Me ! !").css({"transform":"rotateZ(65deg)","font-size":"22px"});
-  });
-
-  
-  //scroll effect 
-  $("body").scroll(function() { 
-    if(sw==true){ setTimeout(function() { fishing(); }, 920); }
-    $("#info").fadeOut(500);
-
-    abOfs = $("#about").offset().top;
-    if( 0<=abOfs){
-      function pl(){
-        $("#ab_box").fadeTo(300,1);
-        for (id=-1; id<=4; id++ ){
-          $("#card li.card_lst:eq("+id+")").css("transition-delay",id*0.2+"s");
-          $("#card li.card_lst:eq("+id+")").addClass("on");
-        } 
-      }setTimeout(function () { pl(); },600);
-    } 
-    
-    alOfs = $("#ability").offset().top;
-    var scr = parseInt($("body").width());
-    if(scr>=1200 && 0!=alOfs){
-      ring_reset();
-    }
-
-    conOfs = $("#contact").offset().top;
-      if(0==conOfs){
-      $("#rocket").css("animation","rocket 8s ease-out infinite");
-    } 
-  });
-  //planets control
-  sw2=true;//점수 멈춤 기능
-  function ring_reset()//
-  { 
-    sw2=false;
-    var scr = parseInt($("body").width());
-    if (scr>=1200){
-      $("#ring_tit").text("Cilck planets.");
-      $("#ability_box li").stop().css("display","none"); 
-      $("#ring_wrap").css({"width":"25vh", "heigth":"25vh", "top":"22%", "left":"15%" });
-      $("#planet_box").css({"width":"87%", "transform":"translate(0%)"});
-    }
-    else {
-      $("#ring_tit").text("Cilck planets.");
-      $("#ability_box li").stop().css("display","none"); 
-    }
-  }
-  // 링클릭시 ability 화면 리셋
-  $("#ring").click(function(){
-    ring_reset();
-  });
-
-  $(".planet_wrap").click(function(){
-    sw2=true;
-    var scr = parseInt($("body").width());
-    if (scr>=1200){
-      $("#ring_wrap").css({"width":"21vh", "heigth":"21vh", "top":"17%", "left":"9%"});
-      $("#planet_box").css({"width":"60%", "transform":"translate(25%)"});
-    } 
-    
-    var name = document.getElementById("ring_tit");
-    var idx = $(this).index();
-    var arr = [95,92,"developing..",88,65,72];
-    var score = arr[idx]; var x = 30;
-    function counter(){
-      if(sw2==false){ return false; }//or x=score;
-      else {
-        if ( typeof score != typeof x ) { //문자일 경우(숫자가 아닐경우)
-          name.innerText = score;
-        } else if( x++<score ){
-          name.innerText = x+"%";
-          setTimeout(counter, 1);
-        }
-      }  
-    } counter();
-    
-    $("#ability_box li.ability_con:eq("+idx+")").stop().slideDown(1000);
-    $("#ability_box li.ability_con:eq("+idx+")").siblings().stop().css("display","none");
   });
 
   //works tab
