c9559a26c90b9d97915047ce6adba8a2db35b1ae
HopeLee <coidss22@gmail.com>
2020-02-29 05:16:51 +0900

    change font

--- m1/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    function ani(){
-        var a = parseInt($(".vs_fr li").width());
-        $(".vs_fr").not(":animated").animate({"margin-left": -a+"px"}, 1000, function(){
-            $(".vs_fr li").eq(0).appendTo($(".vs_fr"));
-            $(".vs_fr").css("margin-left","0");
-        });
-    } setInterval(function(){ ani(); }, 3000);
-});
