c7d11647dd53e2e249a0bdd054f2f4e6a946528d
HopeLee <coidss22@gmail.com>
2020-03-04 01:16:13 +0900

    change section background color

--- hope/data/index.js
+++ hope/data/index.js
@@ -1,44 +1,30 @@
 $(document).ready(function(){
+    function backgroundColor(selector){
+        var mam = document.querySelector(selector),
+        mPoint = mam.getBoundingClientRect(),
+        child = mam.querySelector(".child")
+        cPoint = child.getBoundingClientRect();
+        var radian = Math.atan2(mPoint.height-cPoint.x, mPoint.width-cPoint.x);
+        child.style.transform = 'rotate('+-(180*radian/Math.PI)+'deg)';
+        console.log(mPoint)
+    }
+    backgroundColor('.mam');
 // menu effect
     $(window).resize(function(){
         var wd = $(window).width();
         if ( wd < 959 ){
             $("nav").css("left","100%");
-            $(".menu").removeClass("active");
+            $(".menuBtn").removeClass("active");
         }
         else {
             $("nav").css("left","auto");
         }
+        backgroundColor('.mam');
     });
-    $(window).scroll(function(){
-        var wd = $(window).width();
-        var ht = $(window).height();
-        var st = $(window).scrollTop();
-        if ( st > ht*2/3 ){
-            $("#profileImg, #profileCon").css({"opacity":"1", "transform":"translateX(0)"});
-            if ( wd >= 960 ){
-                $(".gnb a").slice(1).addClass("text_outline");
-            }
-        }
-        else {
-            $(".gnb a").slice(1).removeClass("text_outline");
-        }
-    });
-/*
-// profile page effect( body scroll event isn't working )
-    $("html, body").scroll(function(){
-        var ht = $(window).height();
-        var pbofs = $("#profileBox").offset().top; //윈도우 기준이 아닌 문서 기준의 절대값이 표현되고 있다. 바디 스크롤 이벤트가 안되는 이유와 연관이 있지 않을까?
-        if ( pbofs < ht*2/3 ){
-            $("#profileImg, #profileCon").css({"opacity":"1", "transform":"translateX(0)"});
-        }
-    });
-*/
     var sw = false;
-    $(".menu").click(function(){
+    $(".menuBtn").click(function(){
         if( sw==false ){
             $(this).addClass("active");
-            $(".gnb a").slice(1).removeClass("text_outline");
             $("nav").css("left","0");
         } else {
             $(this).removeClass("active");
@@ -53,6 +39,24 @@ $(document).ready(function(){
             $(".menu").removeClass("active");
         }
     });
+// profile page effect
+    $(window).scroll(function(){
+        var wd = $(window).width();
+        var ht = $(window).height();
+        var st = $(window).scrollTop();
+        if ( st > ht*1/3 ){
+            $("#profileImg, #profileCon").css({"opacity":"1", "transform":"translateX(0)"});
+        }
+    });
+/* ( body scroll event isn't working )
+    $("html, body").scroll(function(){
+        var ht = $(window).height();
+        var pbofs = $("#profileBox").offset().top; //윈도우 기준이 아닌 문서 기준의 절대값이 표현되고 있다. 바디 스크롤 이벤트가 안되는 이유와 연관이 있지 않을까?
+        if ( pbofs < ht*2/3 ){
+            $("#profileImg, #profileCon").css({"opacity":"1", "transform":"translateX(0)"});
+        }
+    });
+*/
 // skill button effect
    $(".skill_btn").click(function(){
         var tar = $(this).find("span");
@@ -70,6 +74,8 @@ $(document).ready(function(){
    });
 // works #kinds btn 
     $("#kinds li").click(function(){
+        $(this).addClass("on")
+        $(this).siblings().removeClass("on")
         var kind = $(this).text();
         $("#list li").css("display","none");
         $("#list li").filter("."+kind).css("display","block");
