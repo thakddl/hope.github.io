329421d7d15506063fc8bcf2ec1c165950650b72
HopeLee <coidss22@gmail.com>
2020-06-13 15:55:38 +0900

    create menuHandler

--- space/js/index.js
+++ space/js/index.js
@@ -1,6 +1,12 @@
 (()=>{
     
+    const nav = document.querySelector('nav');
+    const gnb = document.querySelector('.gnb');
+    const gnbList = gnb.children;
+    const menuBtn = document.querySelector('#icon_moon');
+    
     let currentSection = 0;
+    let menu = false;
 
     const sectionInfo = [
         {//0
@@ -43,10 +49,37 @@
         }
     }
 
+    function menuHandler(){
+        if (!menu){
+            nav.style.transform = 'translateX(0%)';
+            menuBtn.style.animation = 'circle 1.5s 1 ease-out';
+            for( i=0; i<gnbList.length; i++ ){
+                gnbList[i].style.transitionDelay = `${0.2*i}s`;
+                gnbList[i].style.transform = `translate3d(0, 0, 0)`;
+                gnbList[i].style.opacity = 1;
+            }
+            menu = true;
+        } else {
+            menu = false;
+            menuBtn.style.animation = 'moon_scale 5s infinite linear';
+            let reverseNumber = 0;
+            for( i=gnbList.length-1; i>=0; i-- ){
+                reverseNumber++
+                gnbList[i].style.transitionDelay = `${0.2*reverseNumber}s`;
+                gnbList[i].style.transform = `translate3d(300px, 0, 0)`;
+                gnbList[i].style.opacity = 0;
+            }
+            setTimeout(()=>{ 
+                nav.style.transform = 'translateX(100%)';
+            }, 1200);
+        }
+    }
+
     window.addEventListener('resize',()=>{
         setBase();
     });
     window.addEventListener('scroll', ()=>{
         setBase();
     });
+    menuBtn.addEventListener('click', menuHandler);
 })();
