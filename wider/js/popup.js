1794cdc3dd8da999c7ef2abbee681887b095af8f
HopeLee <coidss22@gmail.com>
2020-08-02 18:27:59 +0900

    update data

--- wider/js/popup.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.callPop').click(function(){
-    $(this).siblings('.popup').slideDown(300);
-  });
-  $('.popup .closeBtn').click(function(){
-    $('.popup').slideUp(300);
-  });
-});
